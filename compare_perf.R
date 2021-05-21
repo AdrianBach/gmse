c8d7967c24fd7b2f19608bc39e6d7b2ba8563083
Adrian Bach <adrian.bach@stir.ac.uk>
2021-05-21 17:58:51 +0200

    tiny changes

--- compare_perf.R
+++ compare_perf.R
@@ -44,3 +44,24 @@ write.csv(pred_perf, file = paste(repo,"pred-perf.csv", sep = ""), row.names = F
 
 ext_reg <- length(which(regular[,3] == 0))/dim(regular)[1]
 ext_prd <- length(which(pred[,3] == 0))/dim(pred)[1]
+
+sim <- gmse(time_max = ts, mem_prv_observ = FALSE, traj_pred = FALSE,
+            RESOURCE_ini = popinit, res_birth_type = 0, res_death_type = 0, 
+            consume_surv = surv, consume_repr = repr, times_feeding = tf, res_consume = cons,
+            land_ownership = TRUE, land_dim_1 = ldim, land_dim_2 = ldim,
+            stakeholders = stkh, scaring = FALSE, manager_budget = bdgt, user_budget = bdgt, 
+            manager_sense = 0.15, manage_target = trgt,
+            observe_type = obs, res_move_obs = FALSE,
+            plotting = FALSE)
+
+sim_prd <- gmse(time_max = ts, mem_prv_observ = TRUE, traj_pred = TRUE,
+            RESOURCE_ini = popinit, res_birth_type = 0, res_death_type = 0, 
+            consume_surv = surv, consume_repr = repr, times_feeding = tf, res_consume = cons,
+            land_ownership = TRUE, land_dim_1 = ldim, land_dim_2 = ldim,
+            stakeholders = stkh, scaring = FALSE, manager_budget = bdgt, user_budget = bdgt, 
+            manager_sense = 0.15, manage_target = trgt,
+            observe_type = obs, res_move_obs = FALSE,
+            plotting = FALSE)
+
+plot_gmse_results(sim)
+plot_gmse_results(sim_prd)
